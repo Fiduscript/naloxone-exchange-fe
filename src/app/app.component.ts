41b7ff951fe3573da7f1e5c0b4e2dc877d6a3629
Gavin Shriver <gs@Flame.lan>
2018-06-09 16:46:02 -0700

    installs pug support

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,7 +2,7 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html',
+  templateUrl: './app.component.pug',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
