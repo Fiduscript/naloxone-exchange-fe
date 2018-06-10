30582200d0914e39d020aeed75d42c72f4e0f01a
Gavin Shriver <gs@Flame.lan>
2018-06-09 17:51:34 -0700

    adds basic scaffolding; header, images, search bar

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,7 +3,7 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.pug',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.styl']
 })
 export class AppComponent {
   title = 'app';
