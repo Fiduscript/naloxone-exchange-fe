ce1cf2cb22143f1f41172b128463a745c84ab793
Gavin Shriver <gavin.shriver@gmail.com>
2018-06-23 10:24:32 -0700

    initial commit of product pages

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,17 +7,19 @@ import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
+import { ProductsModule } from './products/products.module';
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    HomeComponent
-  ],
   imports: [
     BrowserModule,
     AngularFontAwesomeModule,
     AppRoutingModule,
     NgbModule.forRoot(),
+    ProductsModule,
+  ],
+  declarations: [
+    AppComponent,
+    HomeComponent
   ],
   providers: [],
   bootstrap: [AppComponent]
