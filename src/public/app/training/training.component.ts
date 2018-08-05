e1a5b4c84f29a9e3516c5741d06e3f8c8472ec64
Victoria Constant <vconstant@uchicago.edu>
2018-08-05 18:03:43 -0500

    Updated training page. Probably has errors.

---
+++ src/public/app/training/training.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-training',
+  templateUrl: './training.component.pug',
+  styleUrls: ['./training.component.styl']
+})
+export class TrainingComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
