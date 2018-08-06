c663e8e99b2d6d5c1060fba257599ca554d92bc2
Gavin Shriver <gavin.shriver@gmail.com>
2018-08-05 19:03:14 -0700

    fixes something

--- src/public/app/app-routing.module.ts
+++ src/public/app/app-routing.module.ts
@@ -1,7 +1,6 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-import { ContactComponent } from './contact/contact.component';
 import { OverdoseComponent } from './overdose/overdose.component';
 
 const routes: Routes = [
