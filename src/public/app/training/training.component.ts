7c1181e1c300d538b38f550d19a1f2dbe29f165b
Victoria Constant <vconstant@uchicago.edu>
2018-08-07 13:15:56 -0500

    Fixed indentation; added scrolling capability. Code is questionable; still figuring this out

--- src/public/app/training/training.component.ts
+++ src/public/app/training/training.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import * as _ from 'lodash';
 
 @Component({
   selector: 'app-training',
@@ -7,9 +9,21 @@ import { Component, OnInit } from '@angular/core';
 })
 export class TrainingComponent implements OnInit {
 
-  constructor() { }
+  constructor(private route: ActivatedRoute) { }
 
   ngOnInit() {
+    //not used yet
+    _.delay(_.bind(this.scrollToAnchor, this), 200);
   }
 
+  private scrollToAnchor(): void {
+    this.route.fragment.subscribe(((hash: string): void => {
+      if(hash) {
+        const cmp = document.getElementById(hash);
+        if (cmp) {
+          cmp.scrollIntoView();
+        }
+      }
+    }))
+  }
 }
