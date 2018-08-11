06a1ece5e6089438f3ea36d777b2ac9c0651a98d
Gavin Shriver <gavin.shriver@gmail.com>
2018-08-11 12:45:53 -0700

    adds css styling as per wireframes

--- src/public/app/about-us/about-us-routing.module.ts
+++ src/public/app/about-us/about-us-routing.module.ts
@@ -2,10 +2,6 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
 import { AboutUsComponent } from './about-us.component';
-import { MissionComponent } from './mission/mission.component';
-import { HistoryComponent } from './history/history.component';
-import { OurTeamComponent } from './our-team/our-team.component';
-import { OurSponsorsComponent } from './our-sponsors/our-sponsors.component';
 
 const routes: Routes = [
   {path: 'about-us', component: AboutUsComponent}
