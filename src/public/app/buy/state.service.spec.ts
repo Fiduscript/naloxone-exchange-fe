0ac455cc4bd5c00d5cd61758a1a748b4603e87e2
Victoria Constant <vconstant@uchicago.edu>
2018-08-19 19:51:27 -0500

    De-linting almost done

--- src/public/app/buy/state.service.spec.ts
+++ src/public/app/buy/state.service.spec.ts
@@ -1,4 +1,4 @@
-import { TestBed, inject } from '@angular/core/testing';
+import { inject, TestBed } from '@angular/core/testing';
 import { StateService } from './state.service';
 
 describe('StateService', () => {
