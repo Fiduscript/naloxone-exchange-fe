4eeb374a6f2010748d7878785a5ec6c72935d1f8
jehwa <36173774+jehwa@users.noreply.github.com>
2018-08-20 18:29:28 -0700

    fixed tests (#61)

--- src/public/app/home/home.component.spec.ts
+++ src/public/app/home/home.component.spec.ts
@@ -2,6 +2,7 @@ import { APP_BASE_HREF } from '@angular/common';
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 import { BrowserModule } from '@angular/platform-browser';
 import { RouterModule } from '@angular/router';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 
 import { HomeComponent } from './home.component';
 
@@ -16,6 +17,7 @@ describe('HomeComponent', () => {
       ],
       imports: [
         BrowserModule,
+        FontAwesomeModule,
         RouterModule.forRoot([])
       ],
       providers: [
