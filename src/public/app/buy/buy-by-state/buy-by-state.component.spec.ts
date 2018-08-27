653ea5220e498d8bac6953e59cea941ec879251d
Gavin Shriver <gavin.shriver@gmail.com>
2018-08-26 22:07:04 -0700

    fixes test

--- src/public/app/buy/buy-by-state/buy-by-state.component.spec.ts
+++ src/public/app/buy/buy-by-state/buy-by-state.component.spec.ts
@@ -1,8 +1,11 @@
 import { APP_BASE_HREF } from '@angular/common';
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 import { RouterModule } from '@angular/router';
+import { MarkdownModule } from 'ngx-markdown';
 
 import { UpdateSubscriberModule } from '../../update-subscriber/update-subscriber.module';
+import { ProductComponent } from '../product/product.component';
+import { ProductsComponent } from '../products/products.component';
 import { BuyByStateComponent } from './buy-by-state.component';
 
 describe('BuyByStateComponent', () => {
@@ -12,11 +15,14 @@ describe('BuyByStateComponent', () => {
   beforeEach(async(() => {
     TestBed.configureTestingModule({
       imports: [
+        MarkdownModule.forRoot(),
         RouterModule.forRoot([]),
         UpdateSubscriberModule
       ],
       declarations: [
-        BuyByStateComponent
+        BuyByStateComponent,
+        ProductComponent,
+        ProductsComponent
       ],
       providers: [
         {provide: APP_BASE_HREF, useValue: '/buy/Alabama'}
