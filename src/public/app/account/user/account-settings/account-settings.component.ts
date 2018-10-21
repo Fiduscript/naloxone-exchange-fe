42fb47b40becad236a2dd22d7167299d68ca6144
Gavin Shriver <5093258+drshriveer@users.noreply.github.com>
2018-10-21 14:08:11 -0700

    Adds basic account settings without edit actions (#100)
    * Adds basic account settings without edit actions
    
    * fixes login exp

--- src/public/app/account/user/account-settings/account-settings.component.ts
+++ src/public/app/account/user/account-settings/account-settings.component.ts
@@ -1,5 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 
+import { LoginService } from '../../login/login.service';
+import { UserInfo } from '../../model/user-info';
+
 @Component({
   selector: 'app-user-account-settings',
   templateUrl: './account-settings.component.pug',
@@ -7,9 +10,17 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AccountSettingsComponent implements OnInit {
 
-  public constructor() { }
+  public user: UserInfo;
+
+  public constructor(private loginService: LoginService) {
+    this.user = new UserInfo();
+  }
 
   public ngOnInit(): void {
+    this.loginService.whoami().subscribe((user: UserInfo) => {
+      // XXX: if user is not logged in should redirect
+      this.user = user;
+    });
   }
 
 }
