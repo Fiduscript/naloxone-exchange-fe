700411095122ebaddd5bece618a26e8a9d256ca9
Ryan Conrad <ryanconrad@Ryans-iMac.local>
2018-12-02 12:03:32 -0500

    merging

--- src/server/dao/users-dao.ts
+++ src/server/dao/users-dao.ts
@@ -3,20 +3,48 @@ import * as _ from 'lodash';
 import {UserAddress} from '../../public/app/account/model/user-address';
 import {Dependent} from '../../public/app/account/model/dependent';
 
+import {DynamoDB} from 'aws-sdk/clients/all';
+import {ErrorMessage, SuccessMessage} from '../../public/app/common/message-response';
+import {AWSError} from 'aws-sdk/lib/error';
+import {AWSProvider} from '../provider/aws-provider';
+
+const TABLE_NAME: string = 'user_addresses';
+const ddb: DynamoDB = AWSProvider.getDynmoClient();
+
 export interface UsersDao {
   getAddressesForUser(id: string): [UserAddress];
+  createAddressForUser(id: string): [UserAddress];
+  deleteAddressForUser(userId: string);
   getDependentsForUser(id: string): [Dependent];
   createDependent(userId: string, dependent: Dependent);
   updateDependent(userId: string, dependent: Dependent);
-  deleteAddressForUser(userId: string);
 }
-export interface UsersDao {
-  getAddressesForUser(id: string): []
-  email: string;
-  privacyAgreement: string;
-}
-class UsersDaoDdb implements UsersDao {
 
+class UsersDaoDdb implements UsersDao {
+  getAddressesForUser(id: string): [UserAddress] {
+    return null;
+  }
+
+  deleteAddressForUser(userId: string): void {
+
+    const params: DynamoDB.DeleteItemInput = {
+      TableName: TABLE_NAME,
+      Key: DynamoDB.Converter.marshall(userId)
+    };
+
+    ddb.deleteItem(params, (err: AWSError, data: DynamoDB.PutItemOutput) => {
+      if (err != null) {
+        log.error(`Failed to unsubscribe customer \`${params}\`.` , err.message, err);
+        res.status(500).json(new ErrorMessage('Unable to unsubscribe at this time. Please try again later.'));
+      } else {
+        res.status(201).json(new SuccessMessage('Successfully unsubscribed!'));
+      }
+    });
+  }
+  const params: DynamoDB.PutItemInput = {
+    TableName: TABLE_NAME,
+    Item: DynamoDB.Converter.marshall(req.body)
+  };
 }
 
 function printLabel(labelledObj: LabelledValue) {
