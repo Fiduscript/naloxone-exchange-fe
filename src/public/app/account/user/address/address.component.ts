9da63f71014427cde0aa4017c70c80b8a4c00f2a
Gavin Shriver <gavin.shriver@gmail.com>
2019-01-06 18:38:36 -0800

    moar style fixes

--- src/public/app/account/user/address/address.component.ts
+++ src/public/app/account/user/address/address.component.ts
@@ -1,5 +1,7 @@
 import { Component, Input, OnInit } from '@angular/core';
+
 import { IAddress } from '../../model/address';
+import { UserService } from '../user.service';
 
 @Component({
   selector: 'app-address',
@@ -12,9 +14,16 @@ export class AddressComponent implements OnInit {
   @Input() public editable: boolean = false;
   // @Input() public selectable: boolean = false;
 
-  public constructor() { }
+  public constructor(private service: UserService) { }
+
+  public deleteAddress(): void {
+    console.log('delete address');
+  }
+
+  public editAddress(): void {
+    console.log('open edit dialog');
+  }
 
   public ngOnInit(): void {
   }
-
 }
