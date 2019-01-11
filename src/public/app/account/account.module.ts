3c142d3725172d2cfcb7f2740b5291ae329d8e50
Gavin Shriver <gavin.shriver@gmail.com>
2019-01-10 22:42:50 -0800

    Adds ability to view, edit, delete, add addresses in profile.

--- src/public/app/account/account.module.ts
+++ src/public/app/account/account.module.ts
@@ -12,6 +12,8 @@ import { LogoutComponent } from './logout/logout.component';
 import { PrivacyComponent } from './privacy/privacy.component';
 import { RegisterComponent } from './register/register.component';
 import { AccountSettingsComponent } from './user/account-settings/account-settings.component';
+import { AddressFormComponent } from './user/address-form/address-form.component';
+import { AddressComponent } from './user/address/address.component';
 import { AddressesComponent } from './user/addresses/addresses.component';
 import { OrdersComponent } from './user/orders/orders.component';
 import { ProfileComponent } from './user/profile/profile.component';
@@ -50,6 +52,8 @@ import { UserAuthGuard } from './user/user-auth.guard';
     PrivacyComponent,
     UpdateAttributeComponent,
     UpdatePasswordComponent,
+    AddressComponent,
+    AddressFormComponent,
   ]
 })
 export class AccountModule { }
--- src/public/app/account/user/user.service.ts
+++ src/public/app/account/user/user.service.ts
@@ -1,4 +1,37 @@
 import { Injectable } from '@angular/core';
+import * as _ from 'lodash';
+import { Observable, of, throwError } from 'rxjs';
+
+import { ErrorMessage, SuccessMessage } from '../../common/message-response';
+import { IAddress } from '../model/address';
+
+const ADDRESSSES: {[key: string]: IAddress} = _.keyBy([
+  {
+    addressId: 'ADDY-123',
+    userId: 'USER-123',
+    name: 'Lewis Black',
+    state: 'Washington',
+    street1: '123 abc st',
+    street2: '',
+    city: 'Seattle',
+    zip: '98121',
+    phoneNumber: '555-444-3333',
+    weekendOkay: true
+  },
+  {
+    addressId: 'ADDY-456',
+    userId: 'USER-123',
+    name: 'Lewis Black c/o Amazon.com',
+    state: 'Colorado',
+    street1: '551 boren ave N',
+    street2: '',
+    city: 'Seattle',
+    zip: '98109',
+    phoneNumber: '555-444-3333',
+    weekendOkay: false,
+    specialInstructions: 'Leave with amazon front desk.'
+  }
+], 'addressId');
 
 @Injectable({
   providedIn: 'root'
@@ -7,8 +40,16 @@ export class UserService {
 
   public constructor() { }
 
-  public getAddressses() {
-    // TODO: implement
+  public deleteAddress(addressId: string): Observable<SuccessMessage> {
+    const success: boolean = delete ADDRESSSES[addressId];
+    if (success) {
+      return of(new SuccessMessage(`Successfully deleted address ${addressId}`));
+    }
+    return throwError(new ErrorMessage(`Failed to deleted address ${addressId}`));
+  }
+
+  public getAddressses(): Observable<IAddress[]> {
+    return of(_.values(ADDRESSSES));
   }
 
   public getOrders() {
@@ -18,5 +59,12 @@ export class UserService {
   public getRelations() {
     // TODO: implement
   }
+
+  public setAddress(address: IAddress): Observable<SuccessMessage> {
+    ADDRESSSES[address.addressId] = address;
+    return of(new SuccessMessage('Success was had here'));
+  }
+
+
 
 }
