5cfb3e8a136fed2201b996d4562145f20abb4c5f
Ryan Conrad <ryan.conrad@fanduel.com>
2019-01-31 19:24:11 +0800

    fixing tests

--- src/public/app/account/user/user.service.spec.ts
+++ src/public/app/account/user/user.service.spec.ts
@@ -1,11 +1,15 @@
-import { inject, TestBed } from '@angular/core/testing';
+import {inject, TestBed} from '@angular/core/testing';
 
-import { UserService } from './user.service';
+import {HttpClientTestingModule} from '@angular/common/http/testing';
+import {UserService} from './user.service';
 
 describe('UserService', () => {
   beforeEach(() => {
     TestBed.configureTestingModule({
-      providers: [UserService]
+      providers: [UserService],
+      imports: [
+        HttpClientTestingModule
+      ]
     });
   });
 
