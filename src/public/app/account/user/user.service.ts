b8992bef72f2700e635ed3cdf398bfea32d400b0
Ryan Conrad <rconrad1@alumni.nd.edu>
2019-03-11 09:01:11 -0400

    fixing user service merge

--- src/public/app/account/user/user.service.ts
+++ src/public/app/account/user/user.service.ts
@@ -2,13 +2,13 @@ import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import * as _ from 'lodash';
 import * as moment from 'moment';
-import { Observable, of } from 'rxjs';
+import { Observable, of, throwError } from 'rxjs';
 import { FiduServiceBase } from '../../common/fidu-service-base';
 import { UserAddress } from '../model/user-address';
 
-import { SuccessMessage } from '../../common/message-response';
+import { ErrorMessage, SuccessMessage } from '../../common/message-response';
 import { jsonConvert } from '../../util/json-convert-provider';
-import { RELATIONS, UserRelation } from './model/user-relation';
+import { RELATIONS, UserRelation, UserRelations } from './model/user-relation';
 
 const relations = _.keyBy([
   {id: '1', birthDate: moment(), name: 'Jake', biologicalSex: 'male',
@@ -36,10 +36,12 @@ export class UserService extends FiduServiceBase {
     return this.http.put<UserAddress>(path, {userId: address.userId, addressId: address.addressId});
   }
 
-  public getAddresses(): Observable<UserAddress[]> {
-    const path: string = '/api/users/getAddresses/';
-
-    return this.http.get<UserAddress[]>(path);
+  public deleteRelation(relationId: string): Observable<SuccessMessage> {
+    const success: boolean = delete relations[relationId];
+    if (success) {
+      return of(new SuccessMessage(`Successfully deleted relation.`));
+    }
+    return throwError(new ErrorMessage(`Failed to deleted relation.`));
   }
 
   public getAddresses(): Observable<UserAddress[]> {
@@ -48,19 +50,13 @@ export class UserService extends FiduServiceBase {
     return this.http.get<UserAddress[]>(path);
   }
 
-
-  public getDependents() {
-    // TODO: implement
-  }
-
   public getOrders() {
     // TODO: implement
     // should this be here? might make more sense to have an OrdersService
   }
 
-  public getOrders() {
-    // TODO: implement
-    // should this be here? might make more sense to have an OrdersService
+  public getRelations(): Observable<UserRelations> {
+    return of(jsonConvert.deserialize({relations: _.values(relations)}, UserRelations));
   }
 
   // TODO return success msg?
