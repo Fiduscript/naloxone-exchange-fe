fa0af4d96543082eb3f4e851a766145ff3ed093b
Balaji <balaji@fiduscript.com>
2019-03-13 23:03:22 -0400

    separate contact us into separate tab in menu

--- src/public/app/app-routing.module.ts
+++ src/public/app/app-routing.module.ts
@@ -1,10 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
+import { ContactComponent } from './about-us/contact/contact.component';
 import { TrainingComponent } from './training/training.component';
 
 const routes: Routes = [
   {path: 'training', component: TrainingComponent},
+  {path: 'contact', component: ContactComponent},
   {path: '**', redirectTo: ''},
 ];
 
